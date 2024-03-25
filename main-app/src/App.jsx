27ff5d63f244c1fc04837eda639d19927d9c0b6d
xWalkeY <pecevsidamjan@gmail.com>
2024-03-25 15:22:25 +0100

    favicon

--- main-app/src/App.jsx
+++ main-app/src/App.jsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/loginForm'; 
 import Header from './components/header';
+import Favicon from './favicon.svg';
 
 function App() {
   return (
